78aae82ecd423e7250c73fb4efbbdb6f3ec7326e
KhangNV2009 <khangnv20.09.2000@gmail.com>
2020-12-19 17:45:35 +0700

    add course UI

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,16 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AcademicManagement.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
+using AcademicManagement.Models;
 
 namespace AcademicManagement.Controllers
 {
     public class CoursesController : Controller
     {
+        public AcademicContext context;
+
+        public CoursesController(AcademicContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            var trainees = this.context.Trainees.ToList();
+            var categories = this.context.Categories.ToList();
+            
+            dynamic models = new ExpandoObject();
+            models.Trainees = trainees;
+            models.Categories = categories;
+            return View(models);
+        }
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Assign(int[] listId, string Name, string Category, string Desciption)
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Category);
+            Console.WriteLine(Desciption);
+
+            foreach(int id in listId)
+            {
+                Console.WriteLine(id);
+            }
+            var trainees = this.context.Trainees.ToList();
+            var categories = this.context.Categories.ToList();
+
+            dynamic models = new ExpandoObject();
+            models.Trainees = trainees;
+            models.Categories = categories;
+            return View(models);
         }
     }
 }
